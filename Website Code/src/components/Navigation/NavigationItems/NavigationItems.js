79e995b21e3328d5803ed45ab70137d03cd13bd1
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-04-23 10:30:01 +0530

    Add files via upload

---
+++ Website Code/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './NavigationItems.module.css'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+const NavigationItems = () => (
+   <ul className={classes.NavigationItems}>
+       <NavigationItem link="/" active>Burger Builder</NavigationItem>
+       <NavigationItem link="/">Checkout</NavigationItem>
+   </ul>
+)
+    
+
+export default NavigationItems
