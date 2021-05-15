073290ca89eccf6a817b7db8e4d09067d1cdbd18
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-05-15 15:31:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          
+          <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route exact path="/" component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -45,25 +45,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading:true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name:'vaibhavi',
-                email:'vaibhavi@gmail',
-                address: {
-                    street:'shejwal park',
-                    zipCode:'46565',
-                    country: 'india'
-                }
-            },
-            deliveryMethod:'fastest'
-        }
-        axios.post('/orders.json',order)
-            .then(response =>this.setState({loading:false, purchasing: false}))
-            .catch(error => this.setState({loading:false, purchasing: false}))
-
+        // this.setState({loading:true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name:'vaibhavi',
+        //         email:'vaibhavi@gmail',
+        //         address: {
+        //             street:'shejwal park',
+        //             zipCode:'46565',
+        //             country: 'india'
+        //         }
+        //     },
+        //     deliveryMethod:'fastest'
+        // }
+        // axios.post('/orders.json',order)
+        //     .then(response =>this.setState({loading:false, purchasing: false}))
+        //     .catch(error => this.setState({loading:false, purchasing: false}))
+        this.props.history.push('/checkout');
     }
 
     purchaseHandler = () => {
