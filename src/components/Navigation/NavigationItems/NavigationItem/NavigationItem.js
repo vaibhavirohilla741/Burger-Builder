5780fe9737abd8af566b7d6361cdd46e54b8205e
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2021-05-17 16:48:03 +0530

    Add files via upload

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import classes from './NavigationItem.module.css'
+import {NavLink} from 'react-router-dom'
 
 const NavigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a 
-            href={props.link}
-            className={props.active ? classes.active : null}>
-            {props.children}
-        </a>
+        <NavLink exact={props.exact}
+            to={props.link}
+            activeClassName={classes.active}>{props.children}
+        </NavLink>
     </li>
 
 ) 
